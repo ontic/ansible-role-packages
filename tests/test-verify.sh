13eaab3f7f2381a58dce96fad8ee7c786ef25c92
Adam Dyson <contact@adamdyson.com.au>
2017-09-28 02:02:56 +1000

    Simplified testing role using Travis CI and docker containers.

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-if [ $distribution = "debian" ] || [ $distribution = "ubuntu" ]; then
+# Verify the installed location.
+docker exec --tty ${container_id} env TERM=xterm which pango-view
+
+# Verify the installed version.
+docker exec --tty ${container_id} env TERM=xterm pango-view --version
+
+# If the distribution is either Debian or Ubuntu.
+if [ "${distribution}" = "debian" ] || [ "${distribution}" = "ubuntu" ]; then
+  # Output all versions and priority for the package.
   docker exec --tty ${container_id} env TERM=xterm apt-cache policy libpango1.0-dev
 fi
-
-docker exec --tty ${container_id} env TERM=xterm pango-view --version
