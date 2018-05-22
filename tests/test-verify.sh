eb33ce2e846f1b5ef4a6dd95fc461c86938a25a9
Adam Dyson <adam.dyson@outlook.com>
2018-05-23 01:08:48 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
 # Verify the installed location.
 docker exec --tty ${container_id} env TERM=xterm which pango-view
-
 # Verify the installed version.
 docker exec --tty ${container_id} env TERM=xterm pango-view --version
 
